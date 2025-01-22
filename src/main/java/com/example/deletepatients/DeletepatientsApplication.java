6ee63c2c04510edfe775fa146ded48d1bc7eed58
DavidSebas20 <davidsebastian20@outlook.com>
2025-01-22 18:58:10 -0500

    :tada: initialize deletepatients project with Spring Boot and Maven setup

---
+++ src/main/java/com/example/deletepatients/DeletepatientsApplication.java
@@ -0,0 +1,13 @@
+package com.example.deletepatients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeletepatientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeletepatientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/deletepatients/DeletepatientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deletepatients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeletepatientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
