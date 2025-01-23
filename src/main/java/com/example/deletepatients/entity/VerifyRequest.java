0e2aa6f81a11f4bc79d49edc777a6468565c74ad
SrNexar <nfsalguero@uce.edu.ec>
2025-01-23 10:16:41 -0500

    ✨ add VerifyRequest and VerifyResponse entities for password verification

---
+++ src/main/java/com/example/deletepatients/entity/VerifyRequest.java
@@ -0,0 +1,29 @@
+package com.example.deletepatients.entity;
+
+public class VerifyRequest {
+    private String password;
+    private String hash;
+
+    public VerifyRequest() {}
+
+    public VerifyRequest(String password, String hash) {
+        this.password = password;
+        this.hash = hash;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+}
